f8c28e495a3dca649a981dfc6d0231cf215b165d
Austin Pierce <Austinentpierce@gmail.com>
2021-09-11 17:47:30 -0400

    little prac to keep basic skills up and running

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
-
 export function App() {
+  for (let step = 0; step < 5; step++) {
+    console.log(`Walking east ${step} step`)
+  }
+
+  for (let thing = 0; thing < 500; thing++) {
+    console.log(`The thing is ${thing}`)
+  }
+  let n = 0
+  while (n < 500) {
+    n++
+  }
+  console.log(n)
   return <div>Hello, World</div>
 }
